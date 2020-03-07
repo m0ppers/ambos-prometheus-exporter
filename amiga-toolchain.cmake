f7dd69213d72e34a5fb51700d8ac8b9929ceec17
Andreas Streichardt <andreas@mop.koeln>
2020-03-08 00:32:37 +0100

    🔧 Optimize setting for gcc

--- amiga-toolchain.cmake
+++ amiga-toolchain.cmake
@@ -8,8 +8,8 @@ SET(CMAKE_CXX_COMPILER "${AMIGA_TOOLCHAIN_PATH}/bin/m68k-amigaos-g++")
 
 set(CMAKE_C_COMPILER_WORKS 1)
 set(CMAKE_CXX_COMPILER_WORKS 1)
-SET(CMAKE_C_FLAGS "-noixemul -m68020 -fomit-frame-pointer -noixemul" CACHE STRING "" FORCE)
-SET(CMAKE_CXX_FLAGS "-noixemul -m68020 -fomit-frame-pointer -noixemul" CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS "-noixemul -m68020 -fomit-frame-pointer -O2" CACHE STRING "" FORCE)
+SET(CMAKE_CXX_FLAGS "-noixemul -m68020 -fomit-frame-pointer -O2" CACHE STRING "" FORCE)
 
 SET(UNIX false)
 SET(AMIGA true)
